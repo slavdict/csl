b0bb1cc709240022530fc63d0b34330199654ad6
Nurono <while0pass@yandex.ru>
2019-01-25 10:43:55 +0300

    Добавлен список основных урлов клиентской навигации

--- src/app.js
+++ src/app.js
@@ -7,13 +7,12 @@ import page from 'page';
 import initKnockout from './scripts/init.knockout.js';
 
 /* eslint-disable-next-line no-constant-condition */
-const baseURL = ('CSL_ENV' !== 'production' ? '' : '');
+const baseURL = 'IS_PRODUCTION' ? 'http://CSL_DOMAIN_NAME/' : '';
 log('CSL portal');
 
 const qs = window.URLSearchParams && (new URLSearchParams(document.location.search));
 window[';)'] = {
   debug: qs && qs.has('debug') || false,
-  debugVideo: qs && qs.has('debugVideo') || false,
 };
 
 function viewModel() {
@@ -24,6 +23,22 @@ const vM = new viewModel();
 initKnockout(ko, vM);
 
 // Настройка клиентской маршрутизации
+const debugURLs = [
+  '/',
+  '/#!dictionary',
+  '/#!dictionary/dict',
+  '/#!dictionary/index',
+  '/#!dictionary/about',
+  '/#!dictionary/-search',
+  '/#!dictionary/-tip',
+  '/#!dictionary/-article',
+  '/#!dictionary/-article-tip',
+  '/#!video',
+  '/#!refs',
+  '/#!feedback',
+];
+/* eslint-disable-next-line no-constant-condition */
+if ('CSL_DEBUG') log(debugURLs);
 
 page.base(baseURL);
 page('/', () => {});
