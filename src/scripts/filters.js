c452e6d7d51a382da4ad7d479a0a843b712500fe
Nurono <while0pass@yandex.ru>
2019-12-01 22:08:23 +0300

    Рантайм-генерация фильтров из статически сгенерированных данных

--- src/scripts/filters.js
+++ src/scripts/filters.js
@@ -1,30 +1,40 @@
-import { filterData } from './filterData.js';
+import { filterData } from '../../.temp/refs/filterData.js';
 import ko from 'knockout';
 
 class FilterGroup {
   constructor(data) {
     this.name = data.name;
-    this.filters = data.filters.map(item => new Filter(item));
+    this.items = data.items.map(item => new Filter(item));
   }
 }
 
 class Filter {
-  constructor(d) {
-    this.name = d.name;
-    this.index = d.index;
+  constructor(data) {
+    this.name = data.name;
+    this.index = data.index;
     this.checked = ko.observable(false);
-    this.groups = d.groups ? d.groups.map(item => new FilterGroup(item)) : [];
-    this.filters = d.filters ? d.filters.map(item => new Filter(item)) : [];
+    this.items = this.getItems(data);
   }
   toggle(filter) {
     filter.checked(!filter.checked());
   }
+  getItems(data) {
+    if (!data.items) return [];
+    return data.items.map(item => {
+      let Constructor = Filter;
+      if (item.items && !item.index) Constructor = FilterGroup;
+      return new Constructor(item);
+    });
+  }
 }
+
+Filter.prototype.isGroup = false;
+FilterGroup.prototype.isGroup = true;
 
 class FilterCategory {
   constructor(data) {
     this.name = data.name;
-    this.filters = data.filters.map(item => new Filter(item));
+    this.items = data.items.map(item => new Filter(item));
   }
 }
 
